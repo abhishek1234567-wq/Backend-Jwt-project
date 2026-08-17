0b8a52dae609fb658a9265dfa7e136ac2871dd0f
abhishek1234567-wq <abhisheksinghrathore153@gmail.com>
2026-08-17 18:04:48 +0530

    frontend url updated

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://frontend-jwt-project.vercel.app/",
+    origin: "https://frontend-jwt-project.vercel.app",
     credentials: true,
   }),
 );
